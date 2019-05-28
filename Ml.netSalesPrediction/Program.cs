8a981f370d52b4f1186da53eb6ae04ee94450925
Salman Chish <13schishti@gmail.com>
2019-05-28 03:02:04 +0100

    add csv text file

---
+++ Ml.netSalesPrediction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ml.netSalesPrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
